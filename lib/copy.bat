e4cea0c6b904037af8ef99d784b0c70ebc599554
Sergey Zhukov <svg@ngs.ru>
2017-01-26 20:50:26 +0700

    Update SS libs

--- lib/copy.bat
+++ lib/copy.bat
@@ -1,27 +1,28 @@
-COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.PostgreSQL\bin\Release\ServiceStack.OrmLite.*
-COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.PostgreSQL\bin\Release\Mono.Security.dll
-COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.PostgreSQL\bin\Release\Npgsql.dll
+SET ROOT=c:\src
+COPY %ROOT%\ServiceStack.OrmLite\src\ServiceStack.OrmLite.PostgreSQL\bin\Release\ServiceStack.OrmLite.*
+COPY %ROOT%\ServiceStack.OrmLite\src\ServiceStack.OrmLite.PostgreSQL\bin\Release\Mono.Security.dll
+COPY %ROOT%\ServiceStack.OrmLite\src\ServiceStack.OrmLite.PostgreSQL\bin\Release\Npgsql.dll
 
-COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.MySql\bin\Release\ServiceStack.OrmLite.MySql.*
-COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.MySql\bin\Release\MySql.Data.*
+COPY %ROOT%\ServiceStack.OrmLite\src\ServiceStack.OrmLite.MySql\bin\Release\ServiceStack.OrmLite.MySql.*
+COPY %ROOT%\ServiceStack.OrmLite\src\ServiceStack.OrmLite.MySql\bin\Release\MySql.Data.*
 
-COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.SqlServer\bin\Release\ServiceStack.OrmLite.SqlServer.*
+COPY %ROOT%\ServiceStack.OrmLite\src\ServiceStack.OrmLite.SqlServer\bin\Release\ServiceStack.OrmLite.SqlServer.*
 
-COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.Sqlite\bin\Release\ServiceStack.OrmLite.Sqlite.*
-COPY C:\src\ServiceStack\lib\Mono.Data.Sqlite.dll .
-COPY C:\src\ServiceStack\lib\sqlite3.dll .
+COPY %ROOT%\ServiceStack.OrmLite\src\ServiceStack.OrmLite.Sqlite\bin\Release\ServiceStack.OrmLite.Sqlite.*
+COPY %ROOT%\ServiceStack\lib\Mono.Data.Sqlite.dll .
+COPY %ROOT%\ServiceStack\lib\sqlite3.dll .
 
-COPY C:\src\ServiceStack.Redis\src\ServiceStack.Redis\bin\Release\ServiceStack.Redis.* .
+COPY %ROOT%\ServiceStack.Redis\src\ServiceStack.Redis\bin\Release\ServiceStack.Redis.* .
 
-COPY C:\src\ServiceStack\src\ServiceStack.Authentication.OAuth2\bin\Release\ServiceStack.Authentication.OAuth2.* .
-COPY C:\src\ServiceStack\src\ServiceStack.Authentication.OAuth2\bin\Release\DotNetOpenAuth.* .
+COPY %ROOT%\ServiceStack\src\ServiceStack.Authentication.OAuth2\bin\Release\ServiceStack.Authentication.OAuth2.* .
+COPY %ROOT%\ServiceStack\src\ServiceStack.Authentication.OAuth2\bin\Release\DotNetOpenAuth.* .
 
-COPY C:\src\ServiceStack\src\ServiceStack.Authentication.OpenId\bin\Release\ServiceStack.Authentication.OpenId.* .
+COPY %ROOT%\ServiceStack\src\ServiceStack.Authentication.OpenId\bin\Release\ServiceStack.Authentication.OpenId.* .
 
-COPY C:\src\ServiceStack\src\ServiceStack.Logging.Log4Net\bin\Release\ServiceStack.Logging.Log4Net.* .
+COPY %ROOT%\ServiceStack\src\ServiceStack.Logging.Log4Net\bin\Release\ServiceStack.Logging.Log4Net.* .
 
-COPY C:\src\ServiceStack\src\ServiceStack.Api.Swagger\bin\Release\ServiceStack.Api.Swagger.* .
-COPY C:\src\ServiceStack\src\ServiceStack.Server\bin\Release\ServiceStack.Server.* .
-COPY C:\src\ServiceStack\src\ServiceStack.Razor\bin\Release\* .
+COPY %ROOT%\ServiceStack\src\ServiceStack.Api.Swagger\bin\Release\ServiceStack.Api.Swagger.* .
+COPY %ROOT%\ServiceStack\src\ServiceStack.Server\bin\Release\ServiceStack.Server.* .
+COPY %ROOT%\ServiceStack\src\ServiceStack.Razor\bin\Release\* .
 
-COPY C:\src\Stripe\src\Stripe\StripeGateway.cs C:\src\Licensing\www.logic\
+COPY %ROOT%\Stripe\src\Stripe\StripeGateway.cs %ROOT%\Licensing\www.logic\
